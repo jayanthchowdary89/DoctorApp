9dbc4d7d352f645f1375d4c4b4a14c8e1324251b
jayanthchowdary89 <jayanthchowdaryy@gmail.com>
2023-11-09 14:58:45 +0530

    Add project files.

---
+++ Models/DoctorAppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DoctorApp.Models
+{
+    public class DoctorAppDbContext : DbContext
+    {
+        public DoctorAppDbContext(DbContextOptions<DoctorAppDbContext> options) :base(options)
+        {
+            
+        }
+
+        public DbSet<Doctor> Doctors { get; set; }
+
+        public DbSet<Patient> Patients { get; set; }
+
+        public DbSet<Appointment> Appointments { get; set; }
+
+
+
+    }
+}
